909c683c7525b9a76a61caca73e1b50961d47b3b
Renato <u201511892@upc.edu.pe>
2022-11-08 21:30:59 -0500

    feat: default values in Leasing

--- src/app/components/leasing/leasing.component.ts
+++ src/app/components/leasing/leasing.component.ts
@@ -1,10 +1,11 @@
+import { Datos } from './../../model/datos.service';
 import {
   FormGroup,
   FormBuilder,
   Validators,
   FormControl,
 } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-leasing',
@@ -12,8 +13,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leasing.component.css'],
 })
 export class LeasingComponent implements OnInit {
+  data: Datos = {
+    PV: 125000,
+    pCI: 20000,
+    NA: 15,
+    frec: 30,
+    NDxA: 360,
+    CostesNotariales: 150,
+    CostesRegistrales: 250,
+    Tasacion: 80,
+    ComisionEstudio: 50,
+    ComisionActivacion: 0,
+    ComPer: 0,
+    PortesPer: 3.5,
+    GasAdmPer: 0,
+    pSegDes: 0.05,
+    pSegRie: 0.3,
+    COK: 25.0,
+  };
   leasingTable: any = [325];
   formGroup!: FormGroup;
+
   constructor(private formBuilder: FormBuilder) {
     this.formGroup = this.formBuilder.group({
       //...del prestamo
@@ -26,26 +46,72 @@ export class LeasingComponent implements OnInit {
         Validators.required,
         Validators.pattern('^[0-9]*$'),
       ]),
-      frecuencia_de_pago: new FormControl('', [Validators.required]),
-      num_dias_por_año: new FormControl('', [Validators.required]),
+      frecuencia_de_pago: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      num_dias_por_año: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
       //...de los costes/gatos iniciales
-      costes_notariales: new FormControl('', [Validators.required]),
-      costes_registrales: new FormControl('', [Validators.required]),
-      tasacion: new FormControl('', [Validators.required]),
-      comision_de_estudio: new FormControl('', [Validators.required]),
-      comision_activación: new FormControl('', [Validators.required]),
+      costes_notariales: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      costes_registrales: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      tasacion: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      comision_de_estudio: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      comision_activación: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
       //...de los costes/gastos periodicos
-      comision_periodica: new FormControl('', [Validators.required]),
-      portes: new FormControl('', [Validators.required]),
-      gastos_de_administración: new FormControl('', [Validators.required]),
+      comision_periodica: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      portes: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      gastos_de_administración: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
       porcentaje_de_seguro_desgravamen: new FormControl('', [
         Validators.required,
+        Validators.pattern('^[0-9]*$'),
       ]),
-      porcentaje_de_seguro_riesgo: new FormControl('', [Validators.required]),
+      porcentaje_de_seguro_riesgo: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
       //...del costo de oportunidad
-      tasa_de_descuento: new FormControl('', [Validators.required]),
+      tasa_de_descuento: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
     });
   }
 
   ngOnInit(): void {}
+
+  Submit() {
+    if (this.formGroup.valid) {
+      this.data = this.formGroup.value;
+      console.log(this.data);
+    } else {
+      console.log(this.data);
+    }
+  }
 }
--- src/app/model/datos.service.ts
+++ src/app/model/datos.service.ts
@@ -1,13 +1,22 @@
 export interface Datos {
+  //Prestamo
   PV: number;
   pCI: number;
   NA: number;
   frec: number;
   NDxA: number;
+  //Costes/Gastos iniciales
+  CostesNotariales: number;
+  CostesRegistrales: number;
+  Tasacion: number;
+  ComisionEstudio: number;
+  ComisionActivacion: number;
+  //Costes/Gastos periodicos
   ComPer: number;
   PortesPer: number;
   GasAdmPer: number;
   pSegDes: number;
   pSegRie: number;
+  //Costo de oportunidad
   COK: number;
 }
