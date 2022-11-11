36c3facfe47b07be35efd689dddca0f6edeb41fb
Renato <u201511892@upc.edu.pe>
2022-11-11 00:27:03 -0500

    feat: leasingTable equations

--- src/app/components/leasing/leasing.component.ts
+++ src/app/components/leasing/leasing.component.ts
@@ -276,6 +276,21 @@ export class LeasingComponent implements OnInit {
         this.leasingTable[i].PG = 'S';
       }
 
+      //--------------------Saldo Inicial --------------------
+      //--------------------Saldo Inicial Indexado --------------------
+      //--------------------Interes --------------------
+      //--------------------Cuota (inc Seg Des) --------------------
+      //--------------------Amortizacion--------------------
+      //--------------------Prepago --------------------
+      //No tiene formula
+      //--------------------Seguro Desgravamen --------------------
+      //--------------------Seguro riesgo--------------------
+      //--------------------Comision --------------------
+      //--------------------Portes --------------------
+      //--------------------Gastos Adm. --------------------
+      //--------------------Saldo Final --------------------
+      //--------------------Flujo --------------------
+
       //...
       this.dataSource = new MatTableDataSource(this.leasingTable);
       console.log(this.leasingTable);
@@ -386,6 +401,170 @@ export class LeasingComponent implements OnInit {
       return Math.pow(1 + TEA, frec / NDxA) - 1;
     } else {
       return 0;
+    }
+  }
+
+  //--------------------Saldo Inicial --------------------
+  SI({
+    NC,
+    Prestamo,
+    N,
+    SaldoFinal,
+  }: {
+    NC: number;
+    Prestamo: number;
+    N: number;
+    SaldoFinal: number;
+  }) {
+    if (NC == 1) {
+      return Prestamo;
+    } else if (NC <= N) {
+      return SaldoFinal;
+    } else {
+      return 0;
+    }
+  }
+
+  //--------------------Saldo Inicial Indexado --------------------
+  SII({ SI, IP }: { SI: number; IP: number }) {
+    return SI + SI * IP;
+  }
+
+  //--------------------Interes --------------------
+  I({ SII, TEP }: { SII: number; TEP: number }) {
+    return -SII * TEP;
+  }
+
+  //--------------------Cuota (inc Seg Des) --------------------
+  Cuota({
+    NC,
+    N,
+    PG,
+    I,
+    A,
+    SegDes,
+  }: {
+    NC: number;
+    N: number;
+    PG: string;
+    I: number;
+    A: number;
+    SegDes: number;
+  }) {
+    if (NC <= N) {
+      if (PG == 'T') {
+        return 0;
+      } else {
+        if ((PG = 'P')) {
+          return I;
+        } else {
+          return I + A + SegDes;
+        }
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  //--------------------Amortizacion--------------------
+  A({ NC, N, PG, SII }: { NC: number; N: number; PG: string; SII: number }) {
+    if (NC <= N) {
+      if (PG == 'T' || PG == 'P') {
+        return 0;
+      } else {
+        return -SII / (N - NC + 1);
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  //--------------------Prepago --------------------
+  //PP({}: {}) {}
+  //--------------------Seguro Desgravamen --------------------
+  SegDes({ SII, pSegDesPer }: { SII: number; pSegDesPer: number }) {
+    return -SII * pSegDesPer;
+  }
+
+  //--------------------Seguro riesgo--------------------
+  SegRie({ NC, N, SegRiePer }: { NC: number; N: number; SegRiePer: number }) {
+    if (NC <= N) {
+      return -SegRiePer;
+    } else {
+      return 0;
+    }
+  }
+
+  //--------------------Comision --------------------
+  Comision({ NC, N, ComPer }: { NC: number; N: number; ComPer: number }) {
+    if (NC <= N) {
+      return -ComPer;
+    } else {
+      return 0;
+    }
+  }
+
+  //--------------------Portes --------------------
+  Portes({ NC, N, PortesPer }: { NC: number; N: number; PortesPer: number }) {
+    if (NC <= N) {
+      return -PortesPer;
+    } else {
+      return 0;
+    }
+  }
+
+  //--------------------Gastos Adm. --------------------
+  GasAdm({ NC, N, GasAdmPer }: { NC: number; N: number; GasAdmPer: number }) {
+    if (NC <= N) {
+      return -GasAdmPer;
+    } else {
+      return 0;
+    }
+  }
+  //--------------------Saldo Final --------------------
+  SF({
+    PG,
+    SII,
+    I,
+    A,
+    PP,
+  }: {
+    PG: string;
+    SII: number;
+    I: number;
+    A: number;
+    PP: number;
+  }) {
+    if (PG == 'T') {
+      return SII - I;
+    } else {
+      return SII + A + PP;
+    }
+  }
+  //--------------------Flujo --------------------
+  Flujo({
+    Cuota,
+    PP,
+    SegRie,
+    Comision,
+    Portes,
+    GasAdm,
+    PG,
+    SegDes,
+  }: {
+    Cuota: number;
+    PP: number;
+    SegRie: number;
+    Comision: number;
+    Portes: number;
+    GasAdm: number;
+    PG: string;
+    SegDes: number;
+  }) {
+    if (PG == 'T' || PG == 'P') {
+      return Cuota + PP + SegRie + Comision + Portes + GasAdm + SegDes;
+    } else {
+      return Cuota + PP + SegRie + Comision + Portes + GasAdm;
     }
   }
 }
