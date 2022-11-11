405328c809fc30bedfeebc70acae73c2c7700c20
Renato <u201511892@upc.edu.pe>
2022-11-10 21:13:03 -0500

    chore: merge dev v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import { LeasingComponent } from './components/leasing/leasing.component';
-=======
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -10,28 +7,15 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { LeasingComponent } from './components/leasing/leasing.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
->>>>>>> 2f6a7ab0fd4a635bad260e44eb74c0159e1906de
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { LoginComponent } from './components/login/login.component';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { SharedModule } from './components/shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatTableModule } from '@angular/material/table';
 
->>>>>>> 2f6a7ab0fd4a635bad260e44eb74c0159e1906de
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,12 +27,9 @@ import { MatTableModule } from '@angular/material/table';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-=======
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
->>>>>>> 2f6a7ab0fd4a635bad260e44eb74c0159e1906de
     SharedModule,
     HttpClientModule,
     FormsModule,
